8d70c4e945568073d429b2a03b28b4e3d5211e7b
Karlis <karlis.developer@gmail.com>
2021-10-20 11:06:08 +0300

    Initial Commit

---
+++ Memorize Stanford CS193p/Memorize_Stanford_CS193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_Stanford_CS193pApp.swift
+//  Memorize Stanford CS193p
+//
+//  Created by karlis.stekels on 20/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_Stanford_CS193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
